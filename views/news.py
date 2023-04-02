949948e54f1cfaf27c8abd06edfa808e827ece78
Jhenil Parihar <jhenilparihar@gmail.com>
2023-04-02 16:20:51 +0530

    api

--- views/news.py
+++ views/news.py
@@ -11,7 +11,7 @@ def open_file(filepath):
         return infile.read()
 
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
